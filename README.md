f032f5cc22edb4869c0407e0d6a1602e13d7f506
mpedramfar <m.pedramfar15@alumni.imperial.ac.uk>
2022-07-09 15:07:09 -0400

    Add several customization options and better error handling. Update README

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ This is done using [Zotero translators](https://www.zotero.org/support/translato
 
 ## Instalation
 
+### Zotero translation server
 Download and run the [Zotero translation server](https://github.com/zotero/translation-server/).
 Note that this library tries to connect to this server and does not work when the server is not running.
+
+**Method 1: Using docker (easier to set up but uses more reources)**
+
 The easiest way to install this is to install [docker](https://docs.docker.com/engine/install/) and run
 ```bash
 docker pull zotero/translation-server
@@ -16,7 +20,11 @@ If you want the translation server to always run (so that you wouldn't have to r
 ```bash
 docker run --restart=always -d -p 1969:1969 --name translation-server zotero/translation-server
 ```
+**Method 2: Using nodejs (more lightweight)**
+
 You can also run the translation server [without docker, using nodejs](https://github.com/zotero/translation-server/).
+
+### Zotra
 
 Clone the repository and add the following line to your init file.
 
@@ -26,22 +34,95 @@ Clone the repository and add the following line to your init file.
 
 ## Usage and configuration
 
-From inside emacs, run `zotra-add-bibtex-entry`, then enter the url and choose the bibtex file to save into.
+From inside emacs, run `zotra-add-entry-from-url`, then enter the url and choose the bibtex file to save into.
+Alternatively, you can run `zotra-add-entry-from-search`, then enter the search identifier (DOI, ISBN, PMID, arXiv ID) and choose the bibtex file to save into.
 
-You can set the variable `zotra-default-bibliography` so that it always saves there.
+Here is the complete list of customizable variables:
 
-You can also set `zotra-after-add-entry-hook` to clean up the recently added entries. For example, if you are using `org-ref`, you could add this line to your init file.
-```emacs-lisp
-(add-hook 'zotra-after-add-entry-hook 'org-ref-clean-bibtex-entry)
-```
+- `zotra-after-add-entry-hook`
+
+  These functions are called after adding an entry. They take no arguments, and they can be used to cleanup and format new entries.
+  For example, if you are using `org-ref`, you could add this line to your init file:
+  ```emacs-lisp
+  (add-hook 'zotra-after-add-entry-hook 'org-ref-clean-bibtex-entry)
+  ```
+  The default value is: '(bibtex-clean-entry)
+
+
+- `zotra-default-bibliography`
+  
+  If this variable is non-nil, interactive calls to `zotra-add-entry-from-url` and `zotra-add-entry-from-search` will not ask the user for the bibfile.
+  Otherwise, zotra will make a list containing the `.bib` files
+  in the current directory and the files returned by
+  `org-cite-list-bibliography-files` and ask the user to choose one."
+
+
+- `zotra-default-entry-format`
+
+   The citation format. Can be one of the following options:
+   + bibtex
+   + biblatex
+   + bookmarks
+   + coins
+   + csljson
+   + csv
+   + endnote_xml
+   + evernote
+   + mods
+   + rdf_bibliontology
+   + rdf_dc
+   + rdf_zotero
+   + refer
+   + refworks_tagged
+   + ris
+   + tei
+   + wikipedia
+   
+   See https://github.com/zotero/translation-server#export-translation
+   and https://github.com/zotero/translation-server/blob/master/src/formats.js
+
+   The default value is: "bibtex"
+
+- `zotra-server-path`
+
+   The url and the port of the Zotero translation server to be used, without a trailing slash mark.
+   
+   The default value is: "http://127.0.0.1:1969"
+
+
+- `zotra-url-cleanup-functions`
+  
+  Currently, the Zotero translation server can’t handle links to pdf files.
+  (See https://github.com/zotero/translation-server/issues/70).
+  These functions provide a way to fix the issue by manually changing the link to a pdf to a link to another url for the article.
+  Each function in this list should take a url and return a url. If the function is not applicable, it should return its input without change.
+
+  Currently, this is implemeted for:
+  + arxiv.org
+
+- `zotra-url-retrieve-timeout`
+   
+   How many seconds to wait for server to get a response.
+   
+   The default value is: 3
+
+- `zotra-use-curl`
+
+  Functions for retrieving citation entries use `url-retrieve-synchronously`, but it sometimes fails. An alternative is to use the external `curl` program to retrieve the data.
+
+  The default value is: nil
+
+
 ### Using zotra with a browser and org-protocol
 
 First you need to set up [org-protocol](https://orgmode.org/worg/org-contrib/org-protocol.html).
 Then make a bookmark in your browser with the following url:
 ```
-javascript:location.href=('org-protocol://zotra?url='+ encodeURIComponent(location.href)+'&bibfile='+encodeURIComponent('/path/to/bibfile.bib')).replace(/'/gi,"%27")
+javascript:location.href=('org-protocol://zotra?url='+ encodeURIComponent(location.href)+'&bibfile='+encodeURIComponent('/path/to/bibfile.bib')+'&format=my-save-format').replace(/'/gi,"%27")
 ```
-If you want to save to `zotra-default-bibliography`, you can simply use:
+where `/path/to/bibfile.bib` should be replaced with the path to the bibfile and `my-save-format` should be replaced with one of the acceptable formats listed above.
+
+You can omit format and bibfile to use the value of `zotra-default-entry-format` and `zotra-default-bibliography` respectively:
 ```
 javascript:location.href=('org-protocol://zotra?url='+ encodeURIComponent(location.href)).replace(/'/gi,"%27")
 ```
--- zotra.el
+++ zotra.el
@@ -19,12 +19,14 @@
 
 ;;; Commentary:
 ;; this library provides functions to connect to a local instance of the Zotero
-;; translation server and fetch bibtex entries using website urls.
+;; translation server and fetch citation information in different formats (e.g. bibtex)
+;; using website urls or search identifiers.
+
 
 ;;; Code:
 (require 'url)
 (require 'org-protocol)
-
+(require 'bibtex)
 
 (defgroup zotra nil
   "Customization group for zotra."
@@ -33,15 +35,30 @@
 
 (defcustom zotra-default-bibliography
   nil
-  "If this value is not nil, entries will only be added here.
-Otherwise, zotra will look at the current directory 
-for `.bib' files and also use `org-cite-list-bibliography-files'."
+  "If this variable is non-nil, interactive calls to
+`zotra-add-entry-from-url' and `zotra-add-entry-from-search'
+will not ask the user for the bibfile.
+Otherwise, zotra will make a list containing the `.bib' files
+in the current directory and the files returned by
+`org-cite-list-bibliography-files' and ask the user to choose one."
   :group 'zotra
   :type 'file)
 
 
+(defcustom zotra-url-cleanup-functions
+  '(zotra-url-cleanup--arxiv)
+  "Currently, the Zotero translation server can't handle links to pdf files.
+(See https://github.com/zotero/translation-server/issues/70).
+These functions provide a way to fix the issue by manually changing the link to
+a pdf to a link to another url for the article.
+Each function in this list should take a url and return a url. If the function is
+not applicable, it should return its input without change."
+  :group 'zotra
+  :type 'hook)
+
+
 (defcustom zotra-after-add-entry-hook
-  nil
+  '(bibtex-clean-entry)
   "These functions are called after adding an entry.
 They take no arguments, and they can be used to
  cleanup and format new entries."
@@ -51,77 +68,152 @@ They take no arguments, and they can be used to
 
 (defcustom zotra-server-path
   "http://127.0.0.1:1969"
-  "The url and the port of the Zotero translation server to be used."
+  "The url and the port of the Zotero translation server to be used, without a trailing slash mark."
   :group 'zotra
   :type 'string)
 
 
 (defcustom zotra-use-curl
   nil
-  "Function for retrieving bibtex entries use `url-retrieve-synchronously',
-but it sometimes fails. An alternative is to use the external `curl' program
-to retrieve the data."
+  "Functions for retrieving citation entries use `url-retrieve-synchronously',
+but it sometimes fails. An alternative is to use the external ‘curl’ program
+ to retrieve the data."
   :group 'zotra
   :type 'boolean)
 
 
-(defun zotra-get-json (url)
-  "Get citation data of URL in Zotero JSON format, using url-retrieve-synchronously
-and Zotero translation server."
-  (let*
-      ((url-request-method "POST")
-       (url-request-extra-headers '(("Content-Type" . "text/plain")))
-       (url-request-data url)
-       (response-buffer (url-retrieve-synchronously
-                         (concat zotra-server-path "/web")))
-       (output (with-current-buffer response-buffer
-                 (goto-char (point-min))
-                 (search-forward "\n\n")
-                 (delete-region (point-min) (point))
-                 (buffer-string))))
-    (kill-buffer response-buffer)
-    (if (equal output "URL not provided")
-        (user-error "URL not provided")
-      output)))
+(defcustom zotra-default-entry-format
+  "bibtex"
+  "The citation format. Can be one of the following options:
+
+bibtex
+biblatex
+bookmarks
+coins
+csljson
+csv
+endnote_xml
+evernote
+mods
+rdf_bibliontology
+rdf_dc
+rdf_zotero
+refer
+refworks_tagged
+ris
+tei
+wikipedia
+
+See https://github.com/zotero/translation-server#export-translation
+and https://github.com/zotero/translation-server/blob/master/src/formats.js"
+  :group 'zotra
+  :type 'string)
 
 
-(defun zotra-get-bibtex-from-json (json)
-  "Convert Zotero JSON format to bibtex, using url-retrieve-synchronously
-and Zotero translation server."
-  (let*
-      ((url-request-method "POST")
-       (url-request-extra-headers '(("Content-Type" . "application/json")))
-       (url-request-data json)
-       (response-buffer (url-retrieve-synchronously
-                         (concat zotra-server-path "/export?format=bibtex")))
-       (output (with-current-buffer response-buffer
-                 (goto-char (point-min))
-                 (search-forward "\n\n")
-                 (delete-region (point-min) (point))
-                 (buffer-string))))
-    (kill-buffer response-buffer)
-    (if (equal output "Bad Request")
-        (user-error "Bad Request")
-      output)))
-
-
-(defun zotra-get-bibtex (url)
-  "Get bibtex data for URL using Zotero translation server."
-  (if zotra-use-curl
-      (shell-command-to-string
-       (format "curl -s -d '%s' -H 'Content-Type: text/plain' '%s/web' | curl -s -d @- -H 'Content-Type: application/json' '%s/export?format=bibtex'"
-               url zotra-server-path zotra-server-path))
-    (zotra-get-bibtex-from-json (zotra-get-json url))))
+(defcustom zotra-url-retrieve-timeout
+  3
+  "How many seconds to wait for server to get a response."
+  :group 'zotra
+  :type 'natnum)
 
 
 
-(defun zotra-add-bibtex-entry (url &optional bibfile)
-  "Get bibtex entry for URL using Zotero translation server. Then
-add it to BIBFILE"
-  (interactive
-   (list (read-string
-          "url: "
-          (ignore-errors (current-kill 0 t)))))
+(defun zotra-run-external-curl (data content-type url)
+  (let* ((curl-cmd-string
+          (format "curl --max-time '%s' -s --show-error -d '%s' -H 'Content-Type: %s' '%s'"
+                  zotra-url-retrieve-timeout data content-type url))
+         (stdout-buffer (get-buffer-create
+                         (generate-new-buffer-name "stdout")))
+         (stderr-buffer (get-buffer-create
+                         (generate-new-buffer-name "stderr")))
+         (return-code (shell-command curl-cmd-string stdout-buffer stderr-buffer))
+         (out (with-current-buffer stdout-buffer (buffer-string)))
+         (err (with-current-buffer stderr-buffer (buffer-string))))
+    (kill-buffer stdout-buffer)
+    (kill-buffer stderr-buffer)
+    (when (not (= return-code 0))
+      (user-error err))
+    out))
+
+
+(defun zotra-get-json (url-or-search-string &optional is-search)
+  "Get citation data of URL-OR-SEARCH-STRING in Zotero JSON format."
+  (let
+      ((json
+        (if zotra-use-curl
+            (zotra-run-external-curl url-or-search-string
+                                     "text/plain"
+                                     (concat zotra-server-path
+                                             (if is-search "/search" "/web")))
+          (let*
+              ((url-request-method "POST")
+               (url-request-extra-headers '(("Content-Type" . "text/plain")))
+               (url-request-data url-or-search-string)
+               (response-buffer (url-retrieve-synchronously
+                                 (concat zotra-server-path
+                                         (if is-search "/search" "/web"))
+                                 nil nil zotra-url-retrieve-timeout))
+               (output
+                (if (null response-buffer)
+                    (user-error "Request failed. If this issue persists, try again with `zotra-use-curl'.")
+                  (with-current-buffer response-buffer
+                    (goto-char (point-min))
+                    (search-forward "\n\n")
+                    (delete-region (point-min) (point))
+                    (buffer-string)))))
+            (kill-buffer response-buffer)
+            output))))
+    (cond ((string= json "URL not provided")
+           (user-error "URL not provided"))
+          ((string= json "No identifiers found")
+           (user-error "No identifiers found"))
+          (t json))))
+
+
+(defun zotra-get-entry-from-json (json &optional entry-format)
+  "Convert Zotero JSON format to ENTRY-FORMAT or `zotra-default-entry-format'
+if ENTR_FORMAT is nil."
+  (let
+      ((entry
+        (if zotra-use-curl
+            (zotra-run-external-curl url-or-search-string
+                                     "application/json"
+                                     (concat zotra-server-path "/export?format="
+                                             (if (null entry-format)
+                                                 zotra-default-entry-format
+                                               entry-format)))
+          (let*
+              ((url-request-method "POST")
+               (url-request-extra-headers '(("Content-Type" . "application/json")))
+               (url-request-data json)
+               (response-buffer (url-retrieve-synchronously
+                                 (concat
+                                  zotra-server-path
+                                  "/export?format="
+                                  (if (null entry-format) zotra-default-entry-format entry-format))
+                                 nil nil zotra-url-retrieve-timeout))
+               (output
+                (if (null response-buffer)
+                    (user-error "Request failed. If this issue persists, try again with `zotra-use-curl'.")
+                  (with-current-buffer response-buffer
+                    (goto-char (point-min))
+                    (search-forward "\n\n")
+                    (delete-region (point-min) (point))
+                    (buffer-string)))))
+            (kill-buffer response-buffer)
+            output))))
+    (cond ((string= entry "Bad Request")
+           (user-error "Bad Request"))
+          ((string= entry "An error occurred during translation. Please check translation with the Zotero client.")
+           (user-error "An error occurred during translation"))
+          (t entry))))
+
+
+(defun zotra-get-entry (url-or-search-string &optional is-search entry-format)
+  (zotra-get-entry-from-json (zotra-get-json url-or-search-string is-search) entry-format))
+
+
+(defun zotra-add-entry (url-or-search-string &optional is-search bibfile entry-format)
   (let ((bibfile
          (or bibfile zotra-default-bibliography
              (completing-read
@@ -132,7 +224,7 @@ add it to BIBFILE"
       (find-file bibfile)
       (goto-char (point-max))
       (when (not (looking-at "^")) (insert "\n"))
-      (insert (zotra-get-bibtex url))
+      (insert (zotra-get-entry url-or-search-string is-search entry-format))
       (save-excursion
         (save-restriction
           (bibtex-narrow-to-entry)
@@ -143,20 +235,71 @@ add it to BIBFILE"
       (save-buffer))))
 
 
+(defun zotra-add-entry-from-url (url &optional bibfile entry-format)
+  "Get citation entry for URL using Zotero translation server. Then
+add it to BIBFILE.
+
+If BIBFILE is nil, use `zotra-default-bibliography'.
+If `zotra-default-bibliography' is nil, ask the user to choose between
+the .bib files in the current directory and the files returned by
+`org-cite-list-bibliography-files'.
+
+ENTRY-FORMAT can take the same values as `zotra-default-entry-format'.
+If ENTRY-FORMAT is nil, use `zotra-default-entry-format'."
+  (interactive
+   (list (read-string
+          "url: "
+          (ignore-errors (current-kill 0 t)))))
+  (mapc (lambda (x)
+          (setq url (funcall x url)))
+        zotra-url-cleanup-functions)
+  (zotra-add-entry url nil bibfile entry-format))
+
+
+(defun zotra-add-entry-from-search (identifier &optional bibfile entry-format)
+  "Get citation entry for search IDENTIFIER (DOI, ISBN, PMID, arXiv ID)
+using Zotero translation server. Then add it to BIBFILE.
+
+If BIBFILE is nil, use `zotra-default-bibliography'.
+If `zotra-default-bibliography' is nil, ask the user to choose between
+the .bib files in the current directory and the files returned by
+`org-cite-list-bibliography-files'.
+
+ENTRY-FORMAT can take the same values as `zotra-default-entry-format'.
+If ENTRY-FORMAT is nil, use `zotra-default-entry-format'."
+  (interactive
+   (list (read-string
+          "search identifier (DOI, ISBN, PMID, arXiv ID): "
+          (ignore-errors (current-kill 0 t)))))
+  (zotra-add-entry identifier t bibfile entry-format))
+
 
 (defun zotra-protocol (info)
   (let ((url (plist-get info :url))
-        (bibfile (plist-get info :bibfile)))
+        (bibfile (plist-get info :bibfile))
+        (entry-format (plist-get info :format)))
     (message (format "Zotra received: `%s' to be saved in `%s'" url bibfile))
-    (zotra-add-bibtex-entry url bibfile)
+    (zotra-add-entry-from-url url bibfile entry-format)
     nil))
-
 
 
 (add-to-list 'org-protocol-protocol-alist
              '("zotra-protocol"
                :protocol "zotra"
                :function zotra-protocol))
+
+
+
+;; zotra-url-cleanup-functions
+
+
+(defun zotra-url-cleanup--arxiv (url)
+  (if (string-match
+       "^\\(https?://\\)?\\(www\.\\)?arxiv.org/pdf/\\(.*\\)\.pdf" url)
+      (concat
+       "https://arxiv.org/abs/"
+       (match-string-no-properties 3 url))
+    url))
 
 
 
